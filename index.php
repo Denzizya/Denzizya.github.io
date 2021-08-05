3f48d3f426b51621886c973bbfbfb3df8d71a506
Denis Fedan <denis1486@gmail.com>
2021-08-05 18:54:40 +0300

    f commit

--- index.php
+++ index.php
@@ -118,3 +118,16 @@
 	
 	file_get_contents($url_telegram . '/sendMessage?chat_id=' . $chat_id . '&text=Не знаю что сказать.');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Приветствую</title>
+</head>
+<body>
+	<div>Приветствую на github</div>
+</body>
+</html>
